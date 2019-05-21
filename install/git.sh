c6c60337b9b7233030b26d65d7043b9915400ed0
zzzeyez <zzzeyez@gmail.com>
2019-05-20 17:16:51 -0700

    new

--- install/git.sh
+++ install/git.sh
@@ -3,7 +3,20 @@
 
 scripts="${HOME}/scripts" 
 
+# color escapes
+red="\e[31m"
+grn="\e[32m"
+ylw="\e[33m"
+blu="\e[34m"
+pur="\e[35m"             
+cyn="\e[36m"                                       
+gry="\e[90m"
+rst="\e[0m"
+bar="▔▔▔▔▔▔"       
+bar="$red$bar$grn$bar$ylw$bar$blu$bar$pur$bar$cyn$bar$rst"
+
 clone() {                                                      
+	printf "\n$1\n$bar\n"
 	if [[ ! -d "$scripts/$1" ]] ; then                         
 		printf "\ncloning $1 into $scripts/$1\n"
 		git clone "https://github.com/zzzeyez/$1.git" "$scripts/$1"
